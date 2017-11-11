6096ceccecbb746b8a6b75d9b9a879570d73da2c
Frederik Ring <frederik.ring@gmail.com>
2017-11-11 13:53:32 +0100

    add event types

--- types.go
+++ types.go
@@ -73,3 +73,49 @@ type UpdateResponse struct {
 	Updated []string `json:"updated"`
 	Status  Status   `json:"status"`
 }
+
+// Event describes an event to be sent to the events API
+type Event struct {
+	APIKey     string          `json:"api_key"`
+	UserID     string          `json:"user_id"`
+	Intent     string          `json:"intent"`
+	TimeStamp  int             `json:"timestamp_millis,omitempty"`
+	Platform   string          `json:"platform,omitempty"`
+	Version    string          `json:"version,omitempty"`
+	Properties []EventProperty `json:"properties"`
+}
+
+// Events is a collection of Event
+type Events []Event
+
+// EventProperty is a property that is attached to an event
+type EventProperty struct {
+	Name         string  `json:"property_name"`
+	StringValue  string  `json:"string_value,omitempty"`
+	IntegerValue int     `json:"integer_value,omitempty"`
+	FloatValue   float64 `json:"float_value,omitempty"`
+	BoolValue    bool    `json:"bool_value,omitempty"`
+}
+
+// NewEventProperty generates an EventProperty containing the correctly
+// typed field for the passed value
+func NewEventProperty(name string, value interface{}) (EventProperty, error) {
+	p := EventProperty{Name: name}
+	if s, ok := value.(string); ok {
+		p.StringValue = s
+		return p, nil
+	}
+	if i, ok := value.(int); ok {
+		p.IntegerValue = i
+		return p, nil
+	}
+	if f, ok := value.(float64); ok {
+		p.FloatValue = f
+		return p, nil
+	}
+	if b, ok := value.(bool); ok {
+		p.BoolValue = b
+		return p, nil
+	}
+	return p, fmt.Errorf("could not use %v as event property value", value)
+}
