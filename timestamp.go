557158045a4708cf1d293f757fb7d72e9ee81878
Frederik Ring <frederik.ring@gmail.com>
2018-03-20 20:18:15 +0100

    change expected timestamp value

--- timestamp.go
+++ timestamp.go
@@ -6,5 +6,5 @@ import (
 
 // TimeStamp returns the current time in UNIX milliseconds
 var TimeStamp = func() int {
-	return int(time.Now().Unix()) / int(time.Microsecond)
+	return int(time.Now().Unix())
 }
